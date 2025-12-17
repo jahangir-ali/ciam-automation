7e4794b399d49667b3fc95c5538c62252c26b4c0
Jahangir Ali <jahangir.ali@easyjet.com>
2025-12-17 19:12:25 +0000

    added tests for prod_RegistrationForm

--- cypress/e2e/51_prod_RegistrationForm.cy.ts
+++ cypress/e2e/51_prod_RegistrationForm.cy.ts
@@ -1,8 +1,12 @@
+import { contains } from "cypress/types/jquery";
+import { keys } from "cypress/types/lodash";
+
 describe('registration spec', () => {
   beforeEach(() => {
     cy.visit("https://www.easyjet.com/en/register");
     cy.window().clearCookies();
     cy.viewport(1280, 1080)
+    //cy.get('#ensCloseBanner').click()
  
   })
 
@@ -11,24 +15,51 @@ describe('registration spec', () => {
 
   })
 
-
+/*
   it('should accept the confirmation on popup modal', () => {
     //cy.on('window:confirm', () => true)
     cy.get('#ensCloseBanner').click()
     //cy.get('cy.get('').should('exist')
   })
+    
 
   it('should fill registration form', () => {
-       cy.get('#ensCloseBanner').click()
+       //cy.get('#ensCloseBanner').click()
     cy.fillRegistrationForm()
   })
+*/
 
-/*
-  it('should display MFA authenticate title', () => {
-    cy.verifyMFAauthenticateTitle();
+  it('should click forgotten password', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('[data-testid="desktop-header-main-menu-test-id"] > .header-main-menu__right-section > .sign-in-button-wrapper > .sign-in-button > .sign-in-button__label').click();
+    cy.get('form').find('> :nth-child(5) > a').click()
+    cy.contains("Please enter the email address you've used when you created your easyJet account. We will then email you a link to reset your password.").should('be.visible');
+
+
   })
 
- */ 
+    it('should click send reset email', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('[data-testid="desktop-header-main-menu-test-id"] > .header-main-menu__right-section > .sign-in-button-wrapper > .sign-in-button > .sign-in-button__label').click();
+    cy.get('form').find('> :nth-child(5) > a').click()
+    
+    cy.get('#email', { includeShadowDom: true}).click()
+    .type('jahangir.ali@easyjet.com')
+    cy.press(Cypress.Keyboard.Keys.TAB);
+    
+    cy.get('#confirmEmail', { includeShadowDom: true})
+    .type('jahangir.ali@easyjet.com');
+    cy.press(Cypress.Keyboard.Keys.TAB)
+    
+
+    //cy.get('button.btn.btn--primary.btn--lg[type="submit"]', { includeShadowDom: true})
+    //.type('{enter}');
+    //.click({ force: true });
+cy.get('button.btn.btn--primary.btn--lg[type="submit"]')
+  .focus()
+  .type('{enter}', { force: true });
+
+ })
 
 })
 
