d61648e682ab853b5dde12f9fe811eb8dd432874
Jahangir Ali <jahangir.ali@easyjet.com>
2025-12-30 16:32:21 +0000

    Completed all languages url tests

--- cypress/e2e/51_prod_SanityTests.cy.ts
+++ cypress/e2e/51_prod_SanityTests.cy.ts
@@ -47,10 +47,18 @@ describe('prod Sign In Tests', () => {
 */
 
   it('should click forgotten password', () => {
-    cy.get('#ensCloseBanner').click()
-    cy.get('[data-testid="desktop-header-main-menu-test-id"] > .header-main-menu__right-section > .sign-in-button-wrapper > .sign-in-button > .sign-in-button__label').click();
-    cy.get('form').find('> :nth-child(5) > a').click()
-    cy.contains("Please enter the email address you've used when you created your easyJet account. We will then email you a link to reset your password.").should('be.visible');
+    cy.get('#ensCloseBanner')
+    .click()
+    
+    cy.get('[data-testid="desktop-header-main-menu-test-id"] > .header-main-menu__right-section > .sign-in-button-wrapper > .sign-in-button > .sign-in-button__label')
+    .click();
+    
+    cy.get('form')
+    .find('> :nth-child(5) > a')
+    .click()
+    
+    cy.contains("Please enter the email address you've used when you created your easyJet account. We will then email you a link to reset your password.")
+    .should('be.visible');
 
 
   })
@@ -71,7 +79,6 @@ describe('prod Sign In Tests', () => {
     .type('{enter}', { force: true })
   
     cy.get('.modal__content > .btn').click();
-   
 
  })
 */
@@ -179,6 +186,296 @@ it('should open and close sign in modal', () => {
 
   })
 
+
+/////// LANGUAGE SANITY TESTS IN ALPHABETICAL ORDER////////////////
+
+it('should confirm CA language url', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('.language-selector')
+    .click()
+
+    //CA
+    cy.get('[for="fc844c50-5b00-4b48-9c53-f3442d019590"]')
+    .click()
+
+    cy.get('.modal__cta-group > .btn--primary')
+    .click()
+
+    cy.url()
+    .should('eq', 'https://www.easyjet.com/ca')
 })
+
+
+it('should confirm CH-FR Swiss language url', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('.language-selector')
+    .click()
+
+    //CH-FR Swiss
+    cy.get('[for="8e7713e6-7963-48a7-9654-51f282776255"]')
+    .click()
+
+    cy.get('.modal__cta-group > .btn--primary')
+    .click()
+
+    cy.url()
+    .should('eq', 'https://www.easyjet.com/ch-fr')
+})
+
+
+it('should confirm CH-DE Swiss language url', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('.language-selector')
+    .click()
+
+    //CH-DE Swiss
+    cy.get('[for="52f30ab3-d867-4b2b-a5e9-58653d503053"]')
+    .click()
+
+    cy.get('.modal__cta-group > .btn--primary')
+    .click()
+
+    cy.url()
+    .should('eq', 'https://www.easyjet.com/ch-de')
+})
+
+
+it('should confirm CS language url', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('.language-selector')
+    .click()
+
+    //CS
+    cy.get('[for="89a24311-5adb-4ce4-90e4-2fddd02089f9"]')
+    .click()
+
+    cy.get('.modal__cta-group > .btn--primary')
+    .click()
+
+    cy.url()
+    .should('eq', 'https://www.easyjet.com/cs')
+})
+
+
+it('should confirm DA language url', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('.language-selector')
+    .click()
+
+    //DA
+    cy.get('[for="74b774bf-e7bc-45b5-9d88-a59dd730b5e8"]')
+    .click()
+
+    cy.get('.modal__cta-group > .btn--primary')
+    .click()
+
+    cy.url()
+    .should('eq', 'https://www.easyjet.com/da')
+})
+
+
+it('should confirm DE language url', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('.language-selector')
+    .click()
+
+    //DE
+    cy.get('[for="d231cba1-e449-4355-89a1-b04a030a19ef"]')
+    .click()
+
+    cy.get('.modal__cta-group > .btn--primary')
+    .click()
+
+    cy.url()
+    .should('eq', 'https://www.easyjet.com/de')
+})
+
+
+it('should confirm EL language url', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('.language-selector')
+    .click()
+
+    //EL
+    cy.get('[for="2f61e504-6945-467e-b7d4-691985407979"]')
+    .click()
+
+    cy.get('.modal__cta-group > .btn--primary')
+    .click()
+
+    cy.url()
+    .should('eq', 'https://www.easyjet.com/el')
+})
+
+
+it('should confirm ES language url', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('.language-selector')
+    .click()
+
+    //ES
+    cy.get('[for="242d8a63-987d-4866-addc-2be1cdb6d892"]')
+    .click()
+
+    cy.get('.modal__cta-group > .btn--primary')
+    .click()
+
+    cy.url()
+    .should('eq', 'https://www.easyjet.com/es')
+})
+
+
+ it('should confirm FR language url', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('.language-selector')
+    .click()
+
+    //FR
+    cy.get('[for="622bbfab-817c-456d-854f-907f132f8d8a"]')
+    .click()
+
+    cy.get('.modal__cta-group > .btn--primary')
+    .click()
+
+    cy.url()
+    .should('eq', 'https://www.easyjet.com/fr')
+})
+
+
+
+it('should confirm HU language url', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('.language-selector')
+    .click()
+
+    //HU
+    cy.get('[for="a87efbd1-2a78-4c4f-b829-10fca1a181db"]')
+    .click()
+
+    cy.get('.modal__cta-group > .btn--primary')
+    .click()
+
+    cy.url()
+    .should('eq', 'https://www.easyjet.com/hu')
+})
+
+
+it('should confirm IL language url', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('.language-selector')
+    .click()
+
+    //IL
+    cy.get('[for="0d52ac9e-63fa-4c8b-b851-079248f00f08"]')
+    .click()
+
+    cy.get('.modal__cta-group > .btn--primary')
+    .click()
+
+    cy.url()
+    .should('eq', 'https://www.easyjet.com/il')
+})
+
+
+it('should confirm IT language url', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('.language-selector')
+    .click()
+
+    //IT
+    cy.get('[for="71f5fea6-4915-4864-8178-5169cc5848ce"]')
+    .click()
+
+    cy.get('.modal__cta-group > .btn--primary')
+    .click()
+
+    cy.url()
+    .should('eq', 'https://www.easyjet.com/it')
+}) 
+
+
+it('should confirm NL language url', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('.language-selector')
+    .click()
+
+    //NL
+    cy.get('[for="75e22c82-912d-4c7f-9594-c161626f133a"]')
+    .click()
+
+    cy.get('.modal__cta-group > .btn--primary')
+    .click()
+
+    cy.url()
+    .should('eq', 'https://www.easyjet.com/nl')
+})
+
+
+
+it('should confirm PL language url', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('.language-selector')
+    .click()
+
+    //PL
+    cy.get('[for="5f9a1100-2de9-4780-b512-c70a57ea9266"]')
+    .click()
+
+    cy.get('.modal__cta-group > .btn--primary')
+    .click()
+
+    cy.url()
+    .should('eq', 'https://www.easyjet.com/pl')
+})
+
+it('should confirm PT language url', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('.language-selector')
+    .click()
+
+    //PT
+    cy.get('[for="19c95416-285b-49d7-ba61-50d1dfbcef3b"]')
+    .click()
+
+    cy.get('.modal__cta-group > .btn--primary')
+    .click()
+
+    cy.url()
+    .should('eq', 'https://www.easyjet.com/pt')
+})
+
+it('should confirm TR language url', () => {
+    cy.get('#ensCloseBanner').click()
+    cy.get('.language-selector')
+    .click()
+
+    //TR
+    cy.get('[for="2c57b060-4ab3-4a90-a9c2-8035886471df"]')
+    .click()
+
+    cy.get('.modal__cta-group > .btn--primary')
+    .click()
+
+    cy.url()
+    .should('eq', 'https://www.easyjet.com/tr')
+})
+
+/////////////////////////////////END OF LANGAGUES //////////////
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
