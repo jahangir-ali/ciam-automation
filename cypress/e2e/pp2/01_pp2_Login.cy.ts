984c7f9a330990fbd7b9b9765481e993102b0d69
Jahangir Ali <jahangir.ali@easyjet.com>
2026-02-17 14:59:38 +0000

    added pp2_Login tests

---
+++ cypress/e2e/pp2/01_pp2_Login.cy.ts
@@ -0,0 +1,43 @@
+import { should } from "chai";
+
+describe('registration spec', () => {
+  beforeEach(() => {
+    cy.visit("https://preprod2.test.easyjet.com/en/");
+    cy.window().clearCookies();
+    cy.viewport(1280, 1080)
+    //cy.get('#ensCloseBanner').click()
+  })
+
+it('should confirm correct url', () => {
+    cy.url().should('eq', 'https://preprod2.test.easyjet.com/en/')
+
+  })
+
+it('should accept the confirmation on popup modal', () => {
+    cy.get('#ensNotifyBannerInner > .ensButtons > #ensAcceptAll')
+  })
+
+it('should trigger "Login Success"', () => {
+    cy.get('#ensNotifyBannerInner > .ensButtons > #ensAcceptAll')
+    .click()
+    
+    cy.get('[data-testid="desktop-header-main-menu-test-id"] > .header-main-menu__right-section > .sign-in-button-wrapper > .sign-in-button')
+    .click();
+    
+    cy.get('#\\34 22a73e2-dc73-44f0-a458-d92da49b8502')
+    .type('jahangir.ali@easyjet.com')
+
+    cy.get('#\\36 838ac1e-0be0-4b4e-b676-5cd0c04fe41e')
+    .type('Testing1234567890')
+
+    cy.get('#KEEP_ME_SIGNED_IN_CHECKED_ID')
+    .click();
+    
+    cy.get('.account-modal__cta')
+    .click();
+    
+    cy.get('[data-testid="dropdown-menu-button"] > span')
+    .should('exist');
+  })
+
+})
