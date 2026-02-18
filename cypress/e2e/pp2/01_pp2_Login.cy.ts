41b3efac20eee521138ea14dbcb15c506a6f03d9
Jahangir Ali <jahangir.ali@easyjet.com>
2026-02-18 17:26:53 +0000

    added login & logout test

--- cypress/e2e/pp2/01_pp2_Login.cy.ts
+++ cypress/e2e/pp2/01_pp2_Login.cy.ts
@@ -17,7 +17,7 @@ it('should accept the confirmation on popup modal', () => {
     cy.get('#ensNotifyBannerInner > .ensButtons > #ensAcceptAll')
   })
 
-it('should trigger "Login Success"', () => {
+it('should trigger "Login to My Account"', () => {
     cy.get('#ensNotifyBannerInner > .ensButtons > #ensAcceptAll')
     .click()
     
@@ -38,6 +38,48 @@ it('should trigger "Login Success"', () => {
     
     cy.get('[data-testid="dropdown-menu-button"] > span')
     .should('exist');
+
+    cy.get('[data-testid="dropdown-menu-button"] > span')
+    .click();
+
+   cy.get('[data-testid="dropdown-menu-link"]')
+  .filter(':contains("My Account")')
+  .click();
+
   })
+
+  
+it.only('Should Login & Logout of account', () => {
+    cy.get('#ensNotifyBannerInner > .ensButtons > #ensAcceptAll')
+    .click()
+    
+    cy.get('[data-testid="desktop-header-main-menu-test-id"] > .header-main-menu__right-section > .sign-in-button-wrapper > .sign-in-button')
+    .click();
+    
+    cy.get('#\\34 22a73e2-dc73-44f0-a458-d92da49b8502')
+    .type('jahangir.ali@easyjet.com')
+
+    cy.get('#\\36 838ac1e-0be0-4b4e-b676-5cd0c04fe41e')
+    .type('Testing1234567890')
+
+    cy.get('#KEEP_ME_SIGNED_IN_CHECKED_ID')
+    .click();
+    
+    cy.get('.account-modal__cta')
+    .click();
+    
+    cy.get('[data-testid="dropdown-menu-button"] > span')
+    .should('exist');
+
+    cy.get('[data-testid="dropdown-menu-button"] > span')
+    .click();
+
+   cy.get('[data-testid="dropdown-menu-link"]')
+  .filter(':contains("Sign Out")')
+  .click();
+
+  })
+
+
 
 })
